b04a1d60ac5a8a25ff46aba88a57e3225cc1b660
Hanyu Wang <1422085086@QQ.com>
2025-08-04 21:06:13 +0800

    yigejianyaochuban

---
+++ v0.0.1/XUETISHUJUKU/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XUETISHUJUKU.MvcApplication" Language="C#" %>
---
+++ v0.0.1/XUETISHUJUKU/Models/User/ApplicationUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace XUETISHUJUKU.Models.User
+{
+    public class ApplicationUser
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string Role { get; set; } // "Student", "Alumni", "Admin"
+
+        // 公共信息
+        public string Phone { get; set; }
+        public string Location { get; set; }
+        public string Bio { get; set; }
+        public string ProfilePictureUrl { get; set; }
+
+        // 关联到校区
+        public int? UniversitycampusID { get; set; }
+        public virtual Universitycampus Universitycampus { get; set; }
+    }
+}
